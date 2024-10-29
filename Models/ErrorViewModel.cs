8c55b0342d163041009dd2221ef9457c6e860ccf
BRUNO NAHUEL MARASI <48113202@ort>
2024-10-29 08:09:49 -0300

    hacemos el mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP09_Login_Marasi_Cordero.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
