bc4a695f944beb85aef3df126ca94eb34924ceb3
BRUNO NAHUEL MARASI <48113202@ort>
2024-11-19 08:15:38 -0300

    nuevaBaseDatos

--- Models/Usuarios.cs
+++ Models/Usuarios.cs
@@ -2,10 +2,12 @@ public class Usuarios
 {
     public string username {get; set;}
     public string contraseña {get;set;}
-    public string nombre {get; set;}
     public string apellido {get;set;}
     public string email {get; set;}
     public int idUsuario {get;set;}
+    public string nombre {get; set;}
+    public string descripcionUsuario {get; set;}
+    public string dirrecionUsuario {get; set;}
     public string nombrePerro {get;set;}
     public Usuarios(){
     }
